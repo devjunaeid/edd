85295e857297c1ee27ff53a06e7816726ccf3e43
devjunaeid <junaeid.md48@gmail.com>
2024-03-22 10:19:42 +0600

    Public photos and general configs

---
+++ next.config.js
@@ -0,0 +1,19 @@
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+      };
+    }
+
+    return config;
+  },
+  experimental: {
+    serverComponentsExternalPackages: ["sequelize"],
+  },
+};
+
+module.exports = nextConfig;
