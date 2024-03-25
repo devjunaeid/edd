e806a235987d76589b62f5efdb25a999cbfa1664
devjunaeid <junaeid.md48@gmail.com>
2024-03-25 12:18:02 +0600

    Postgres SQL Config

---
+++ src/config/psql.config.js
@@ -0,0 +1,15 @@
+const config = {
+    HOST: '65.108.79.152',
+    USER: 'enginee2_eddadmin',
+    PASSWORD: '=uNe!ak3WkG&',
+    DB: 'enginee2_edd',
+    dialect: "postgres",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  };
+  
+  export default config;
