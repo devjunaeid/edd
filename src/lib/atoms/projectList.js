c621f11f7c6abd5bc855042aacb1f9bd3ac3771c
devjunaeid <junaeid.md48@gmail.com>
2024-03-25 12:15:34 +0600

    Recoil Atoms

---
+++ src/lib/atoms/projectList.js
@@ -0,0 +1,5 @@
+import { atom } from "recoil";
+export const projectList = atom({
+  key: "allprojects",
+  default: [],
+  });
---
+++ src/lib/atoms/serviceList.js
@@ -0,0 +1,5 @@
+import { atom } from "recoil";
+export const serviceList = atom({
+  key: "allservice",
+  default: [],
+});
