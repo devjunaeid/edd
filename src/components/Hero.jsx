3fdc245f3de835f9cb92c9e7941234d57fab3646
devjunaeid <junaeid.md48@gmail.com>
2024-04-22 22:49:20 +0600

    Added: NewsLatter Component.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./component.css";
-import Link from "next/link";
 import Image from "next/image";
 import heroImg from "../../public/hero/h3.png";
 import { IoIosArrowDropdown } from "react-icons/io";
+import SubToNewsLatter from "./NewsLatter/SubToNewsLatter";
 
 function Hero() {
   return (
@@ -13,16 +13,19 @@ function Hero() {
       </div>
       <div className="hero_sd">
         <p>
-          We, wellcome you to one of the best architectural and structual consaltration
-          of Gazipur. We always push the limits and creative boundaries. With us
-          you can make your dream reality.
+          We, wellcome you to one of the best architectural and structual
+          consaltration of Gazipur. We always push the limits and creative
+          boundaries. With us you can make your dream reality.
         </p>
-        <a href={"#services"} className="service_link">
-          Check out our services
-          <IoIosArrowDropdown size={20} />
-        </a>
+        <div className="heroLinks">
+          <SubToNewsLatter />
+          <a href={"#services"} className="service_link">
+            Check out our services
+            <IoIosArrowDropdown size={20} />
+          </a>
+        </div>
       </div>
-      <Image src={heroImg} className="hero_img" />
+      <Image src={heroImg} className="hero_img" alt="Hero Img" />
     </div>
   );
 }
